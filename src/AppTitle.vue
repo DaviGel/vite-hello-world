f29bd2f0f4a6a57975bc2dfc93650d2a5f398109
DaviGel <geloso.davide@gmail.com>
2023-12-06 17:36:48 +0100

    feat: esercizio completo + bonus

---
+++ src/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHero from './components/AppHero.vue';
+export default {
+  data() {
+    return {
+      title: 'La mia prima app con Vite!',
+    };
+  },
+  components: {
+    AppHero,
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <AppHero />
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'Ciao, siamo la classe 110!',
+      paragraph:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi accusamus culpa commodi rem recusandae hic quod ullam, labore, vitae quaerat explicabo cum reprehenderit laborum voluptatibus molestias, laudantium porro tenetur soluta.',
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <p>{{ paragraph }}</p>
+</template>
+
+<style scoped></style>
